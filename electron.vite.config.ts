5315c1360f473067d881cad305d481c6dffbeee9
tobynetizen <tobias.hand.0@protonmail.com>
2026-09-23 15:29:37 -0500

    chore: scaffold electron project

---
+++ electron.vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'electron-vite'
+import { resolve } from 'node:path'
+
+export default defineConfig({
+  main: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload/index.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    root: 'src/renderer'
+  }
+})
